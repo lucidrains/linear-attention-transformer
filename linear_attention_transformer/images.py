c129867066578ed88b7e802c3719b42b4a132c5d
Phil Wang <lucidrains@gmail.com>
2020-06-11 10:22:25 -0700

    make ImageLinearAttention behave more like conv class

--- linear_attention_transformer/images.py
+++ linear_attention_transformer/images.py
@@ -2,23 +2,26 @@ import torch
 from torch import nn
 
 class ImageLinearAttention(nn.Module):
-    def __init__(self, chan, key_dim = 64, value_dim = 64, heads = 8):
+    def __init__(self, chan, chan_out = None, kernel_size = 1, padding = 0, stride = 1, key_dim = 64, value_dim = 64, heads = 8):
         super().__init__()
 
         self.chan = chan
+        chan_out = chan if chan_out is None else chan_out
+
         self.key_dim = key_dim
         self.value_dim = value_dim
         self.heads = heads
         
-        self.to_q = nn.Conv2d(chan, key_dim * heads, 1)
-        self.to_k = nn.Conv2d(chan, key_dim, 1)
-        self.to_v = nn.Conv2d(chan, value_dim, 1)
-        self.to_out = nn.Conv2d(value_dim * heads, chan, 1)
+        conv_kwargs = {'padding': padding, 'stride': stride}
+        self.to_q = nn.Conv2d(chan, key_dim * heads, kernel_size, **conv_kwargs)
+        self.to_k = nn.Conv2d(chan, key_dim, kernel_size, **conv_kwargs)
+        self.to_v = nn.Conv2d(chan, value_dim, kernel_size, **conv_kwargs)
+        self.to_out = nn.Conv2d(value_dim * heads, chan_out, kernel_size, **conv_kwargs)
 
     def forward(self, x):
-        b, _, h, w = x.shape
-
         q, k, v = (self.to_q(x), self.to_k(x), self.to_v(x))
+
+        b, _, h, w = q.shape
 
         q = q.reshape(b, self.heads, -1, h * w)
         k = k.reshape(b, -1, h * w)
