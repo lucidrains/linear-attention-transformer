f04e0077c5dae59973f4ab232fbaa0b32b37a80e
Phil Wang <lucidrains@gmail.com>
2020-06-12 12:21:37 -0700

    add ability for ImageLinearAttention to accept contextual key values

--- linear_attention_transformer/images.py
+++ linear_attention_transformer/images.py
@@ -16,16 +16,25 @@ class ImageLinearAttention(nn.Module):
         self.to_q = nn.Conv2d(chan, key_dim * heads, kernel_size, **conv_kwargs)
         self.to_k = nn.Conv2d(chan, key_dim, kernel_size, **conv_kwargs)
         self.to_v = nn.Conv2d(chan, value_dim, kernel_size, **conv_kwargs)
-        self.to_out = nn.Conv2d(value_dim * heads, chan_out, kernel_size, **conv_kwargs)
 
-    def forward(self, x):
+        out_conv_kwargs = {'padding': padding}
+        self.to_out = nn.Conv2d(value_dim * heads, chan_out, kernel_size, **out_conv_kwargs)
+
+    def forward(self, x, context = None):
+        b, c, h, w, k_dim = *x.shape, self.key_dim
+
         q, k, v = (self.to_q(x), self.to_k(x), self.to_v(x))
-
-        b, _, h, w = q.shape
 
         q = q.reshape(b, self.heads, -1, h * w)
         k = k.reshape(b, -1, h * w)
         v = v.reshape(b, -1, h * w)
+
+        if context is not None:
+            context = context.reshape(b, c, 1, -1)
+            ck = self.to_k(context).reshape(b, k_dim, -1)
+            cv = self.to_v(context).reshape(b, k_dim, -1)
+            k = torch.cat((k, ck), dim=2)
+            v = torch.cat((v, cv), dim=2)
 
         k = k.softmax(dim=2)
         q = q.softmax(dim=2)
