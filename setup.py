98efbd6010e494258395e630100f111a77858d3b
Phil Wang <lucidrains@gmail.com>
2020-06-28 17:17:56 -0700

    make sure linformer is added to requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'linear_attention_transformer',
   packages = find_packages(exclude=['examples']),
-  version = '0.9.0',
+  version = '0.9.1',
   license='MIT',
   description = 'Linear Attention Transformer',
   author = 'Phil Wang',
@@ -13,7 +13,8 @@ setup(
   install_requires=[
       'torch',
       'axial-positional-embedding',
-      'product-key-memory>=0.1.5'
+      'product-key-memory>=0.1.5',
+      'linformer'
   ],
   classifiers=[
       'Development Status :: 4 - Beta',
