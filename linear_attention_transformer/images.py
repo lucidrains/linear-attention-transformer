6d21ef080ed09c3d5e052fa5929b0556229ad0ed
Phil Wang <lucidrains@gmail.com>
2020-10-04 21:06:36 -0700

    allow one to turn off normalization of queries for image linear attention, given reported better results from https://openreview.net/forum?id=KiFeuZu24k

--- linear_attention_transformer/images.py
+++ linear_attention_transformer/images.py
@@ -2,16 +2,17 @@ import torch
 from torch import nn
 
 class ImageLinearAttention(nn.Module):
-    def __init__(self, chan, chan_out = None, kernel_size = 1, padding = 0, stride = 1, key_dim = 64, value_dim = 64, heads = 8):
+    def __init__(self, chan, chan_out = None, kernel_size = 1, padding = 0, stride = 1, key_dim = 64, value_dim = 64, heads = 8, norm_queries = True):
         super().__init__()
-
         self.chan = chan
         chan_out = chan if chan_out is None else chan_out
 
         self.key_dim = key_dim
         self.value_dim = value_dim
         self.heads = heads
-        
+
+        self.norm_queries = norm_queries
+
         conv_kwargs = {'padding': padding, 'stride': stride}
         self.to_q = nn.Conv2d(chan, key_dim * heads, kernel_size, **conv_kwargs)
         self.to_k = nn.Conv2d(chan, key_dim * heads, kernel_size, **conv_kwargs)
@@ -37,7 +38,9 @@ class ImageLinearAttention(nn.Module):
             v = torch.cat((v, cv), dim=3)
 
         k = k.softmax(dim=2)
-        q = q.softmax(dim=2)
+
+        if self.norm_queries:
+            q = q.softmax(dim=1)
 
         context = torch.einsum('bhdn,bhen->bhde', k, v)
         out = torch.einsum('bhdn,bhde->bhen', q, context)
