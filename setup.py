24ecf20b11a7c8ddbc15e33a30f0be0cc73b145d
Phil Wang <lucidrains@gmail.com>
2021-09-07 13:15:30 -0700

    tweak eps for causal linear attn

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'linear_attention_transformer',
   packages = find_packages(exclude=['examples']),
-  version = '0.19.0',
+  version = '0.19.1',
   license='MIT',
   description = 'Linear Attention Transformer',
   author = 'Phil Wang',
